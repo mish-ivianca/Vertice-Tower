5a88490b756b4191b68e1b13024e389eb84f7288
Mishel Ivianca <ivianca.cat00@gmail.com>
2026-09-08 20:30:28 -0400

    corrigiendo proyecto para produccion
    :

--- backend/showroom/settings.py
+++ backend/showroom/settings.py
@@ -230,19 +230,11 @@ SIMPLE_JWT = {
 # ============================================================
 
 if not DEBUG:
-
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
     SESSION_COOKIE_SECURE = True
-
     CSRF_COOKIE_SECURE = True
-
     SECURE_SSL_REDIRECT = True
-
     SECURE_HSTS_SECONDS = 31536000
-
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-
     SECURE_HSTS_PRELOAD = True
-
     SECURE_CONTENT_TYPE_NOSNIFF = True
